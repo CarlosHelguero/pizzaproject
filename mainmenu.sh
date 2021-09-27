7425dedbd4116e4dab5f30ca92def6baf0171798
JSmith <65369149+JHussle@users.noreply.github.com>
2021-09-27 08:56:14 -0500

    changes to the menu selection

---
+++ mainmenu.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+echo "Please take a moment to take a look at our main menu. "
+echo ""
+
+
+echo "Is there anything that I can get for you today?"
+echo ""
+
+echo "1. Small Pizza."
+echo "2. Medium Pizza."
+echo "2. Large Pizza."
+echo "2. Extra Large Pizza."
+
+echo ""
+read -p "What pizza size would you like today? [ 1-4 ]: " size
+echo ""
+
+
